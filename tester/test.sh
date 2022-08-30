fb73577d91f01da043f11a2f41f9f64d898ac953
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-08-30 02:48:08 -0300

    Change name to tester

--- tester/test.sh
+++ tester/test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-CONTAINER_NAME="tester_container"
+CONTAINER_NAME="tester"
 NETWORK="sistemas-distribuidos-tp0-base_testing_net"
 
 if [ ! "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
   if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
     docker start $CONTAINER_NAME
   else
-    docker run -idt --name $CONTAINER_NAME --network=$NETWORK tester
+    docker run -dt --name $CONTAINER_NAME --network=$NETWORK tester
   fi
 fi
 
 EXPECTED="TEST"
-TEST=$(docker exec -i tester_container sh -c "echo -n $EXPECTED | nc server 12345")
+TEST=$(docker exec -i $CONTAINER_NAME sh -c "echo -n $EXPECTED | nc server 12345")
 IFS=: read message result <<< "$TEST"
 RESULT=$(echo "$result" | xargs)
 
