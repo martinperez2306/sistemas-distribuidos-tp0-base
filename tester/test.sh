0db6634022e09c9951c19e9cfb6315157c4310cd
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-08-30 02:43:19 -0300

    Add tester for server

---
+++ tester/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CONTAINER_NAME="tester_container"
+NETWORK="sistemas-distribuidos-tp0-base_testing_net"
+
+if [ ! "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+  if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    docker start $CONTAINER_NAME
+  else
+    docker run -idt --name $CONTAINER_NAME --network=$NETWORK tester
+  fi
+fi
+
+EXPECTED="TEST"
+TEST=$(docker exec -i tester_container sh -c "echo -n $EXPECTED | nc server 12345")
+IFS=: read message result <<< "$TEST"
+RESULT=$(echo "$result" | xargs)
+
+if [ "$RESULT" = "$EXPECTED" ]; then
+    echo "Server is working correctly"
+else
+    echo "Server is NOT working correctly"
+fi
