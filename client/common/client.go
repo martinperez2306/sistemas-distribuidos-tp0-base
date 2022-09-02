49843ed64c4de0b558b3341d91ecaf62f56992bb
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-02 02:47:07 -0300

    Add go rutine with loop and listen chanel to break loop

--- client/common/client.go
+++ client/common/client.go
@@ -22,15 +22,17 @@ type ClientConfig struct {
 
 // Client Entity that encapsulates how
 type Client struct {
-	config ClientConfig
-	conn   net.Conn
+	config  ClientConfig
+	conn    net.Conn
+	running bool
 }
 
 // NewClient Initializes a new client receiving the configuration
 // as a parameter
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
-		config: config,
+		config:  config,
+		running: true,
 	}
 	return client
 }
@@ -63,50 +65,55 @@ func (c *Client) StartClientLoop() {
 	signal.Notify(cancelChan, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-	loop:
-		// Send messages if the loopLapse threshold has been not surpassed
-		for timeout := time.After(c.config.LoopLapse); ; {
-			select {
-			case <-timeout:
-				break loop
-			default:
-			}
-
-			// Send
-			fmt.Fprintf(
-				c.conn,
-				"[CLIENT %v] Message N°%v sent\n",
-				c.config.ID,
-				msgID,
-			)
-			msg, err := bufio.NewReader(c.conn).ReadString('\n')
-			msgID++
-
-			if err != nil {
-				log.Errorf(
-					"[CLIENT %v] Error reading from socket. %v.",
-					c.config.ID,
-					err,
-				)
-				c.conn.Close()
-				return
-			}
-			log.Infof("[CLIENT %v] Message from server: %v", c.config.ID, msg)
-
-			// Wait a time between sending one message and the next one
-			time.Sleep(c.config.LoopPeriod)
-
-			// Recreate connection to the server
-			c.conn.Close()
-			c.createClientSocket()
-		}
-
-		log.Infof("[CLIENT %v] Closing connection", c.config.ID)
-		c.conn.Close()
+		sig := <-cancelChan
+		c.running = false
+		log.Infof("[CLIENT %v] Caught shutdown signal %v", sig, c.config.ID)
+		log.Infof("[CLIENT %v]Proceed to shutdown client gracefully")
 	}()
 
-	sig := <-cancelChan
-	log.Infof("Caught shutdown signal %v", sig)
-	log.Infof("Proceed to shutdown client gracefully")
+loop:
+	// Send messages if the loopLapse threshold has been not surpassed
+	for timeout := time.After(c.config.LoopLapse); ; {
+		select {
+		case <-timeout:
+			break loop
+		default:
+		}
+
+		if !c.running {
+			break loop
+		}
+
+		// Send
+		fmt.Fprintf(
+			c.conn,
+			"[CLIENT %v] Message N°%v sent\n",
+			c.config.ID,
+			msgID,
+		)
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		msgID++
+
+		if err != nil {
+			log.Errorf(
+				"[CLIENT %v] Error reading from socket. %v.",
+				c.config.ID,
+				err,
+			)
+			c.conn.Close()
+			return
+		}
+		log.Infof("[CLIENT %v] Message from server: %v", c.config.ID, msg)
+
+		// Wait a time between sending one message and the next one
+		time.Sleep(c.config.LoopPeriod)
+
+		// Recreate connection to the server
+		c.conn.Close()
+		c.createClientSocket()
+	}
+
+	log.Infof("[CLIENT %v] Closing connection", c.config.ID)
 	c.conn.Close()
+	log.Infof("[CLIENT %v] Client stop running", c.config.ID)
 }
