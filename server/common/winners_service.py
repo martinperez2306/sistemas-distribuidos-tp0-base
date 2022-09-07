7c5f0d5eec9b35f46b04a6788c2332f0dae71f19
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-07 13:24:42 -0300

    Rename winners service

--- server/common/winners_service.py
+++ server/common/winners_service.py
@@ -5,7 +5,7 @@ from .utils import Contestant
 from .utils import *
 from datetime import datetime
 
-BATCH_SEPARATOR = "\n"
+BATCH_SEPARATOR = "&"
 
 PLAYER_DATA_SIZE = 4
 
@@ -17,31 +17,31 @@ class WinnerService:
     def get_winners_response(self, message: str):
         logging.info("Get winners response")
         logging.debug("Get winners response. MSG: {}".format(message))
-        contestants = self.get_contestants(message)
-        winners = self.get_winners(contestants)
+        contestants = self.__get_contestants(message)
+        winners = self.__get_winners(contestants)
         logging.debug("Get winners response. Winners: {}".format(winners))
-        self.save_winners(winners)
-        response = self.get_winners_string(winners)
+        self.__save_winners(winners)
+        response = self.__get_winners_string(winners)
         logging.info("Get winners response. Response: {}".format(response))
         return response
 
-    def get_contestants(self, message: str):
+    def __get_contestants(self, message: str):
         logging.info("Get Contestants.".format(message))
         logging.debug("Get Contestants. MSG: {}".format(message))
-        player_list = self.parse_batch_message(message)
+        player_list = self.__parse_batch_message(message)
         contestant_list = []
         for player_msg in player_list:
-            contestant = self.parse_contestant_message(player_msg)
+            contestant = self.__parse_contestant_message(player_msg)
             if (contestant):
                 contestant_list.append(contestant)
         return contestant_list
 
-    def parse_batch_message(self, batch_message: str):
+    def __parse_batch_message(self, batch_message: str):
         parsed = batch_message.split(BATCH_SEPARATOR)
         return parsed
 
 
-    def parse_contestant_message(self, contestant_message: str):
+    def __parse_contestant_message(self, contestant_message: str):
         split = contestant_message.split(CONTESTANT_SEPARATOR)
         if (len(split) == PLAYER_DATA_SIZE):
             first_name = split[0]
@@ -55,7 +55,7 @@ class WinnerService:
                 logging.info("The string is not a date with format " + CONTESTANT_BIRTHDATE_FORMAT)
         return None
 
-    def get_winners(self, contestants: 'list[Contestant]'):
+    def __get_winners(self, contestants: 'list[Contestant]'):
         logging.info("Get winners.")
         winner_list = []
         for contestant in contestants:
@@ -66,7 +66,7 @@ class WinnerService:
                 winner_list.append(contestant)
         return winner_list
 
-    def save_winners(self, winners: 'list[Contestant]'):
+    def __save_winners(self, winners: 'list[Contestant]'):
         logging.info("Save winners")
         logging.debug("Save {} winners".format(len(winners)))
         if winners:
@@ -74,7 +74,7 @@ class WinnerService:
             persist_winners(winners)
             self._winners_semaphore.release()
 
-    def get_winners_string(self, winners: 'list[Contestant]'):
+    def __get_winners_string(self, winners: 'list[Contestant]'):
         string = ""
         for winner in winners:
             string += winner.to_string()
