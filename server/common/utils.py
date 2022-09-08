847fedae1b5b061481ff37a89a09faefb2be1b33
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-08 03:03:51 -0300

    Restore simulation of process

--- server/common/utils.py
+++ server/common/utils.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 
 """ Winners storage location. """
@@ -30,7 +31,7 @@ class Contestant:
 """ Checks whether a contestant is a winner or not. """
 def is_winner(contestant: Contestant) -> bool:
 	# Simulate strong computation requirements using a sleep to increase function retention and force concurrency.
-	#time.sleep(0.001)
+	time.sleep(0.001)
 	return hash(contestant) % 17 == 0
 
 
